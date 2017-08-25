346aeafeda1de73088c42e1679b962cb908d7ec7
Ethan Gutmann <gutmann@ucar.edu>
2017-08-25 10:19:44 -0600

    added robustness to expand_and_thin
    No longer assumes temp directory is local.  But tries to make it local
    if it is being created in /tmp/ to avoid running out of space on /tmp

--- expand_and_thin_file.sh
+++ expand_and_thin_file.sh
@@ -2,22 +2,28 @@
 
 if [[ $1 ]]; then
 
-    TEMP_DIR=`mktemp -d`
-    mkdir -p $TEMP_DIR
+    # no longer assume that mktemp creates a directory in the current directory
+    # so save the current directory for later use.
+    base_dir=`pwd -P`
+
+    MASTER_TEMP_DIR=`mktemp -d`
+    TEMP_DIR=${MASTER_TEMP_DIR#/tmp/}
+    mkdir -p $TEMP_DIR # it should already exist, but incase mktemp doesn't create it
     cd $TEMP_DIR
 
-    mv ../$1 ./
+    mv ${base_dir}/$1 ./
     tar -xf $1
 
     find . -iname '*.nc' -exec mv {} ./ \;
 
-    ls -1 *.nc 2>/dev/null | xargs -n 1 ../../thin_file.sh
+    ls -1 *.nc 2>/dev/null | xargs -n 1 ${base_dir}/../thin_file.sh
 
-    mkdir -p ../nc_output
-    mv subset/*.nc ../nc_output
+    mkdir -p ${base_dir}/nc_output
+    mv subset/*.nc ${base_dir}/nc_output
 
-    cd ../
+    cd ${base_dir}
     rm -r $TEMP_DIR
+    rm -r $MASTER_TEMP_DIR
 
     mkdir -p subset
 
